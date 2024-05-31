0208075f0be7037ecb125dfa4961c74a86034a70
LalaHuseynova <151177723+LalaHuseynova@users.noreply.github.com>
2024-06-01 00:10:33 +0400

    Starting point of project

---
+++ Main.java
@@ -0,0 +1,19 @@
+
+//import LoginSign.LanguageSelector;
+
+import loginSign.LanguageGUI;
+
+import javax.swing.ImageIcon;
+
+
+public class Main {
+    public static void main(String[] args) {
+        ImageIcon img = new ImageIcon("src/icon4.jpg");
+
+        LanguageGUI languageSelector = new LanguageGUI();
+        languageSelector.selectLanguageAndOpenLogin();
+         
+
+       }
+
+}
